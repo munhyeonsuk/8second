960fb63090e2c3e75ecdb7928417e8b12c420698
munhyeonsuk <m01024038735@gmail.com>
2024-08-13 18:20:17 +0900

    신상품, 랭킹 tab

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    $('#newTab').on('click', 'li:not(.on) button', function(){
-        $(this).parent().addClass('on').siblings().removeClass('on');
+    // $('#newTab').on('click', 'li:not(.on) button', function(){
+    //     $(this).parent().addClass('on').siblings().removeClass('on');
+    // })
+
+
+
+    $(".tabcontent .tabbtn a").on('click',function(e){
+        
+        e.preventDefault();
+        const targetTab = $(this).attr('href'); // #w_cont , #m_cont
+        console.log(targetTab)
+
+        $(targetTab).addClass('d-block').siblings().removeClass('d-block');
+
+        
+
     })
 })
